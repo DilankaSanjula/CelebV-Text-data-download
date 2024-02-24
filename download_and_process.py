7da03bc74f6da6c106a6c273b28b70173df0c7fb
DilankaSanjula <dilankasanjula@gmail.com>
2024-02-24 18:48:12 +0530

    FIrst commit

---
+++ download_and_process.py
@@ -0,0 +1,150 @@
+import os
+import json
+import cv2
+from pytube import YouTube, Stream
+import time
+import subprocess
+
+count = 0 
+
+def download(video_path, ytb_id, proxy=None):
+
+    """
+    ytb_id: youtube_id
+    save_folder: save video folder
+    proxy: proxy url, defalut None
+    """
+    if proxy is not None:
+        proxy_cmd = "--proxy {}".format(proxy)
+    else:
+        proxy_cmd = ""
+
+
+    try:
+        link = f"https://www.youtube.com/watch?v={ytb_id}"
+        video = YouTube(link)
+        #file_size = video.streams.get_highest_resolution().filesize
+        stream = video.streams.get_by_itag(137)
+        #print(f"YT Video ID: {ytb_id} | YT Video size: {file_size}")
+
+        if stream and not os.path.exists(video_path):
+            file_size = stream.filesize
+            if file_size < 50000000:
+                print(file_size)
+
+                down_video = " ".join([
+                    "yt-dlp",
+                    proxy_cmd,
+                    "-f", "137",
+                    "https://www.youtube.com/watch?v=" + ytb_id,
+                    "--skip-unavailable-fragments",
+                    "--merge-output-format", 'mp4',
+                    "--output", video_path,
+                    "--external-downloader", "aria2c",
+                    "--external-downloader-args", '"-x 16 -k 1M"'
+                    "--list-formats"
+                ])
+
+                print(down_video)
+                status = os.system(down_video)
+                if status != 0:
+                    print(f"video not found: {ytb_id}")
+
+
+        else:
+            print("Stream with format code 137 not found.")
+            
+    except Exception as exp:
+        print("skipping cannot get yt video file size", exp)
+
+def secs_to_timestr(secs):
+    hrs = secs // (60 * 60)
+    min = (secs - hrs * 3600) // 60 
+    sec = secs % 60
+    end = (secs - int(secs)) * 100
+    return "{:02d}:{:02d}:{:02d}.{:02d}".format(int(hrs), int(min),
+                                                int(sec), int(end))
+
+def expand(bbox, ratio):
+    top, bottom = max(bbox[0] - ratio, 0), min(bbox[1] + ratio, 1)
+    left, right = max(bbox[2] - ratio, 0), min(bbox[3] + ratio, 1)
+
+    return top, bottom, left, right
+
+def to_square(bbox):
+    top, bottom, leftx, right = bbox
+    h = bottom - top
+    w = right - leftx
+    c = min(h, w) // 2
+    c_h = (top + bottom) / 2
+    c_w = (leftx + right) / 2
+
+    top, bottom = c_h - c, c_h + c
+    leftx, right = c_w - c, c_w + c
+    return top, bottom, leftx, right
+
+def denorm(bbox, height, width):
+    top, bottom, left, right = \
+        round(bbox[0] * height), \
+        round(bbox[1] * height), \
+        round(bbox[2] * width), \
+        round(bbox[3] * width)
+
+    return top, bottom, left, right
+
+def process_ffmpeg(raw_vid_path, save_folder, save_vid_name,
+                   bbox, time):
+    """
+    raw_vid_path:
+    save_folder:
+    save_vid_name:
+    bbox: format: top, bottom, left, right. the values are normalized to 0~1
+    time: begin_sec, end_sec
+    """
+
+   
+
+    out_path = os.path.join(save_folder, save_vid_name)
+
+    cap = cv2.VideoCapture(raw_vid_path)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    top, bottom, left, right = to_square(
+        denorm(expand(bbox, 0.02), height, width))
+    start_sec, end_sec = time
+
+    cmd = f"ffmpeg -i {raw_vid_path} -vf crop=w={right-left}:h={bottom-top}:x={left}:y={top} -ss {secs_to_timestr(start_sec)} -to {secs_to_timestr(end_sec)} -loglevel error {out_path}"
+    os.system(cmd)
+    return out_path
+
+
+def load_data(file_path):
+    with open(file_path) as f:
+        data_dict = json.load(f)
+
+    for key, val in data_dict['clips'].items():
+        save_name = key+".mp4"
+        ytb_id = val['ytb_id']
+        time = val['duration']['start_sec'], val['duration']['end_sec']
+
+        bbox = [val['bbox']['top'], val['bbox']['bottom'],
+                val['bbox']['left'], val['bbox']['right']]
+        yield ytb_id, save_name, time, bbox
+
+
+if __name__ == '__main__':
+    json_path = 'celebvtext_info.json'  # json file path
+    raw_vid_root = 'downloaded_celebvhq/raw/'  # download raw video path
+    processed_vid_root = 'downloaded_celebvhq/processed/'  # processed video path
+    proxy = None  # proxy url example, set to None if not use
+
+    os.makedirs(raw_vid_root, exist_ok=True)
+    os.makedirs(processed_vid_root, exist_ok=True)
+
+    for vid_id, save_vid_name, time, bbox in load_data(json_path):
+
+        raw_vid_path = f"downloaded_celebvhq/raw/{vid_id}.mp4"
+        
+        download(raw_vid_path, vid_id, proxy)
+        #process_ffmpeg(raw_vid_path, processed_vid_root, save_vid_name, bbox, time)
+
